0682f3b810fbcd4239aa9410084dfce15d788255
sanghun219 <sanghun219@naver.com>
2021-12-04 23:27:30 +0900

    test

---
+++ public_html/web/back/gateway.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'require.php';
+
+try{
+   // post_data 받기
+   // gateway를 지나면서 이 사람이 세션 연결됐는지 어떻게 확인할것인가? => memcached 이용?
+   session_start();
+   session_regenerate_id(true);
+   $post_data = json_decode(file_get_contents('php://input'));
+   echo json_encode($post_data);
+   
+} catch (Exception $e){
+
+}
