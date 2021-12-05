71055e9fbb795b890cf621fcf033bb0fe2fcf92b
sanghun219 <sanghun219@naver.com>
2021-12-05 16:12:08 +0900

    환경설정 테스트 2

--- public_html/web/back/gateway.php
+++ public_html/web/back/gateway.php
@@ -1,16 +1,15 @@
 <?php
 require_once 'require.php';
+require_once '../vendor/autoload.php';
 
- try{
-    // post_data 받기
-    // gateway를 지나면서 이 사람이 세션 연결됐는지 어떻게 확인할것인가? => memcached 이용?
-    session_start();
-    session_regenerate_id(true);
-    $post_data = json_decode(file_get_contents('php://input'));
-    echo json_encode($post_data);
+try{
+   // post_data 받기
+   // gateway를 지나면서 이 사람이 세션 연결됐는지 어떻게 확인할것인가? => memcached 이용?
+   session_start();
+   session_regenerate_id(true);
+   $post_data = json_decode(file_get_contents('php://input'));
+   echo json_encode($post_data);
    
- } catch (Exception $e){
+} catch (Exception $e){
 
- }
-
-phpinfo();
+}
